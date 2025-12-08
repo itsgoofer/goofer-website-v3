64e32e23b2747caa96a3e6f1a9a9141e652ffa33
Goofer <693696+itsgoofer@users.noreply.github.com>
2025-12-08 12:55:41 +0100

    Update about-section.svelte

--- src/lib/components/sections/about-section.svelte
+++ src/lib/components/sections/about-section.svelte
@@ -10,14 +10,14 @@
 
   <div class="max-w-[760px]">
     <p class="">
-      Hey there. I am Goofer and I create characters and props for both stylized and realistic worlds. My work is all about strong shapes, clear design and personality. I love exploring how form, color and detail can tell a story even before a character moves or speaks. I enjoy building pieces that feel alive and believable, with attention on silhouette, expression and visual impact.
+      I create characters and props for both stylized and realistic worlds. My work is all about strong shapes, clear design and personality. I love exploring how form, color and detail can tell a story even before a character moves or speaks. I enjoy building pieces that feel alive and believable, with attention on silhouette, expression and visual impact.
     </p>
 
     <p class="">
-      When I am not working on art I spend time with music, aviation and coding. These interests keep my mind fresh and push me to look at creativity from different angles.
+      When I am not working on art I spend time with playing music, aviation and coding. These interests keep my mind fresh and push me to look at creativity from different angles.
     </p>
     <p class="">
-      Thanks for visiting.
+      Thanks for visiting!
     </p>
   </div>
 </section>
